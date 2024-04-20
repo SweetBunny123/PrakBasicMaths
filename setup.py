40d37ec14aa2eb6cb200187329e6f743115ed17e
Prakrititz Borah <143780392+SweetBunny123@users.noreply.github.com>
2024-04-20 15:16:26 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PrakBasicMaths',
+    version='0.1',
+    packages=find_packages(),
+    description='A Python package for basic mathematical operations',
+    author='Your Name',
+    author_email='Prakrititz.Borah@iiitb.ac.in',
+    url='https://github.com/SweetBunny123/PrakBasicMaths',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
